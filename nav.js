e852bb4ad30ffda4f3ba77dafcbc067c77c29d59
Mr-SimpleEditor <Mr-SimpleEditor@protonmail.com>
2022-06-04 00:20:56 +0530

    updated

---
+++ nav.js
@@ -0,0 +1,9 @@
+fetch('nav.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#myTopnav");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})
+
